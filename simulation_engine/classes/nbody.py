583412b85cb8022b13322d29f4b1da4ddcdd9a44
benw000 <benjamin.winstanley.007@gmail.com>
2025-05-14 08:38:06 +0100

    example mode file with setup and draw_backdrop_plt functions

--- simulation_engine/classes/nbody.py
+++ simulation_engine/classes/nbody.py
@@ -2,10 +2,81 @@ import copy
 import numpy as np
 from scipy.stats import loguniform
 
-from .parents import Particle
+from simulation_engine.classes.parents import Particle, Environment
+from simulation_engine.utils.manager import Manager
+
+def draw_backdrop_plt(ax):
+    # Get an ax from manager, and plot things on it related to this mode
+    # Overrides Manager.default_draw_backdrop_plt
+    # TODO: specify window name to ax - maybe ax.get_fig.canvas.set_window_title(window_title)
+    window_title = f'N-body animation, {num} bodies'
+
+    # Set padded limits
+    ax.set_xlim(-1, Particle.env_x_lim+1)
+    ax.set_ylim(-1, Particle.env_y_lim+1)
+
+    # Black background
+    ax.set_facecolor('k')
+    return ax
+
+def setup(num_time_steps, delta_t, nums, store_history, save_json, save_mp4, user_csv_path, user_mp4_path):
+    # ---- MANAGER ----
+    # TODO: Update these arguments when certain on the usage cases
+    # TODO: Specify default save names like 'nbody_{nums[0]}_...'
+    # Like this:
+    csv_path = f"Simulation_CSVs/{type}_{str(num)}_{str(now.time())}_{str(now.date())}.csv"
+    mp4_path = f"Simulation_mp4s/{type}_{str(num)}_{str(now.time())}_{str(now.date())}.MP4"
+
+    # Feed args into new manager instance
+    manager = Manager(
+        mode = "nbody",
+        delta_t = delta_t,
+        num_timesteps=num_time_steps,
+        store_history = store_history,
+        save_json = save_json,
+        save_mp4 = save_mp4,
+    )
+
+    # ---- BACKDROP ----
+    manager.draw_backdrop_plt_func = draw_backdrop_plt
 
 
+    # ---- ENVIRONMENT ----
+    # Link all future environment objects to manager
+    Environment.manager = manager
+    # TODO: Initialise environment object list and add to manager state dict
+    # Follow particles for example
 
+
+    # ---- PARTICLES ----
+    # Link all future particles to manager
+    Particle.manager = manager 
+
+    # Set Particle geometry attributes
+    Particle.env_x_lim = 1000
+    Particle.env_y_lim = 1000
+    Particle.track_com = False
+    Particle.torus = False
+
+    # Initialise particles - could hide this in Particle but nice to be explicit
+    num_stars = nums[1]
+    star_list = []
+    random = True
+    if random:
+        # Generate a random set of stars
+        for i in range(num_stars):
+            star_list.append(Star())
+    else:
+        # Hardcode a specific set of stars
+        Star(np.array([300,300]),np.array([200,100]))
+        Star(np.array([600,500]),np.array([-100,300]))
+        Star(np.array([400,700]),np.array([0,-100]))
+
+    # Add list of particles to manager's state dictionary
+    manager.state["Particle"]["Star"] = star_list
+
+    return manager
+    
 class Star(Particle):
     '''
     Star particle for N-body simulation
@@ -33,10 +104,6 @@ class Star(Particle):
 
         # Random gray colour for plotting between 0.5 and 1
         self.colour = np.random.rand()/2 + 0.5
-
-        # Ensure prototype for child class exists, callable by its name as a string only
-        prototype = copy.copy(self)
-        Particle.prototypes[self.__class__.__name__] = prototype
 
     def create_instance(self, id):
         ''' Used to create instance of the same class as self, without referencing class. '''
