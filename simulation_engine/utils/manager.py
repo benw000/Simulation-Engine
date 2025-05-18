dca2ea4b934c052162ff2d43fb6220b43da93665
benw000 <benjamin.winstanley.007@gmail.com>
2025-05-18 22:59:33 +0100

    more fixes and clearing up FuncAnimation logic

--- simulation_engine/utils/manager.py
+++ simulation_engine/utils/manager.py
@@ -14,7 +14,6 @@ class Manager:
 
     # ---- INITIALISATION ---- 
 
-
     # TODO: Clear up inputs
     def __init__(self,
                  delta_t:float = 0.01,
@@ -22,6 +21,9 @@ class Manager:
                  store_history:bool=True,
                  save_json:bool = True,
                  json_path:Path = None,
+                 save_mp4:bool = True,
+                 mp4_path:Path = None,
+                 compute_and_animate:bool = True,
                  show_graph:bool = False,
                  torus:bool = False,
                  track_com:bool = False):
@@ -37,12 +39,12 @@ class Manager:
         self.state = {"Particle":{},
                       "Environment":{}}
         
+        # Initialise max IDs for particle
+        self.max_ids_dict = {}
+
         # Initialise history
         self.store_history = store_history
         self.history = []
-
-        # Initialise kill counting
-        self.kill_tracker = {}
 
         # Time
         self.delta_t = delta_t
@@ -53,15 +55,30 @@ class Manager:
         # Graph
         self.show_graph = show_graph
 
-        # Set up JSON logging path
+        # Logging
+        self.compute_and_animate = compute_and_animate
+        self.save_json = save_json
+        self.save_mp4 = save_mp4
         self.json_path = json_path
-        if json_path is None:
-            self.json_path = Path(f"simulation_{str(datetime.datetime.now())}.json")
-
+        self.mp4_path = mp4_path
+        if self.json_path is None and self.save_json:
+            self.json_path = Path(f"simulation_log_{str(datetime.datetime.now())}.json")
+        if self.mp4_path is None and self.save_mp4:
+            self.json_path = Path(f"simulation_vid_{str(datetime.datetime.now())}.mp4")
+        
     # =============================================================
 
     # ---- DUNDER ---- 
     def __iter__(self):
+        for class_name, class_dict in self.state["Particle"].items():
+            print("DEBUG")
+            print(class_name)
+            print(class_dict)
+            for id, particle in class_dict.items():
+                if particle.alive:
+                    yield particle
+
+    def iterate_all_particles(self):
         for class_name, class_dict in self.state["Particle"].items():
             for id, particle in class_dict.items():
                 if particle.alive:
@@ -100,34 +117,43 @@ class Manager:
             # Can we append to JSON without having to read it? 
             # Could we store in SQL db?
             pass
-        
+    
+    # ---- LOADING ----
+    def load_timestep(self, timestep):
+        # Try loading state from history, if fails try from JSON
+        if self.store_history:
+            self.state = self.history[timestep]
+        # Load from JSON
+        else:
+            # TODO: Read from JSON when history not saved
+            raise NotImplementedError("Reading state from JSON not implemented yet")
+
     # =============================================================
     
     # ---- MATPLOTLIB PLOTTING ----
 
-    # Animation
+    # ---- ANIMATION ----
     def animate_plt(self):
-        # TODO: Fix, this was just copy and paste:
-        # Animate frames by calling update() function
-        interval_between_frames = Particle.delta_t*1000 # milliseconds
-        if show_graph:
-            ani = FuncAnimation(fig, Particle.animate_timestep, frames=time_steps, \
-                            fargs=([ax],[ax2]), interval=interval_between_frames)
-        else:
-            ani = FuncAnimation(fig, Particle.animate_timestep, frames=time_steps, \
-                            fargs=([ax],), interval=interval_between_frames)
+        # Set up figure
+        fig, ax, ax2 = self.setup_figure_plt()
 
-        save_as_mp4 = True
-        if save_as_mp4:
-            mp4_path = mp4_path.replace(":","-")
-            if user_mp4_path is not None:
-                mp4_path = user_mp4_path
-            fps = 1/(interval_between_frames*(10**(-3))) # period -> frequency
-            ani.save(mp4_path, writer='ffmpeg', fps=fps)
+        # Setup a matplotlib FuncAnimation of draw_figure_plt
+        # (draw_figure_plt handles updating/loading state)
+        interval_between_frames = self.delta_t*1000 # milliseconds
+        ani = FuncAnimation(fig, self.draw_figure_plt,
+                            frames=self.num_timesteps,
+                            fargs=([ax],[ax2]), 
+                            interval=interval_between_frames)
+        plt.show()
+
+        # Optionally save video
+        if self.save_mp4:
+            fps = 1/self.delta_t # period -> frequency
+            ani.save(self.mp4_path, writer='ffmpeg', fps=fps)
             print("\n")
-            print(f"Saved simulation as mp4 at {mp4_path}.")
+            print(f"Saved simulation as mp4 at {self.mp4_path}.")
 
-    # Setup figure
+    # ---- SETUP FIGURE ----
     def setup_figure_plt(self):
         # Initialise as None
         fig, ax, ax2 = None, None, None
@@ -150,39 +176,23 @@ class Manager:
             # Adjust spacing between plots if necessary
             plt.subplots_adjust(wspace=0.3)
 
-            # Initialise by plotting empty
-            scat = ax.scatter([], [])
-            scat = ax2.scatter([], []) # Initialise something on it - do we need this?
+            # Initialise ax2 by plotting dimensions
+            max_time = int(self.num_timesteps)*self.delta_t
+            ax2.set_xlim(0, max_time) 
+
         else:
             # Setup figure
             fig, ax = plt.subplots()#figsize=[20,15])
             fig.set_size_inches(20,20)
 
-            # Initialise by plotting empty
-            scat = ax.scatter([], [])
+        # Initialise ax by plotting dimensions
+        ax.set(xlim=[0,Particle.env_x_lim], ylim=[0,Particle.env_x_lim])
         
         # Add title
         window_title = "Simulation" # TODO: change this
         fig.canvas.set_window_title(window_title)
 
-        # Set tight layout
-        fig.tight_layout()
-
-        return fig, ax, ax2
-    
-    def draw_figure_plt(self,ax,ax2):
-        # Draw frame and graph onto existing axes
-        self.draw_frame_plt(ax)
-        if not ax2 is None:
-            self.draw_graph_plt(ax2)
-        
-
-    # Draw frame
-    def draw_frame_plt(self, ax):
-        '''
-        Draw all objects in the simulation's current state onto a supplied matplotlib ax object.
-        '''
-        # ---- SETUP ----
+        # Set layout for ax
         ax.clear()
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
@@ -191,22 +201,64 @@ class Manager:
         ax.xaxis.set_ticks([])
         ax.yaxis.set_ticks([])
         ax.set_aspect('equal', adjustable='datalim')
-        
+        fig.tight_layout()
 
+        # Draw backdrop with either default or user supplied backdrop
+        ax = self.draw_backdrop_plt_func(ax)
+
+        # Initialise the matplotlib 'artists' for each particle and background sprite
+        ax = self.setup_frame_plt(ax)
+        return fig, ax, ax2
+    
+    def setup_frame_plt(self, ax):
         # Decide if tracking the COM in each frame
         com, scene_scale = None, None
         if Particle.track_com:
             com = Particle.centre_of_mass()
             scene_scale = Particle.scene_scale()
 
-        # ---- BACKDROP ----
-        # Update with either default or user supplied backdrop
-        ax = self.draw_backdrop_plt_func(ax)
+        # Update particle and environment artists
+        for environment_object_type, environment_objects_list in self.state["Environment"].items():
+            for environment_object in environment_objects_list:
+                environment_object.init_plt(ax, com,scene_scale)
+        for particle in self.iterate_all_particles():
+            particle.init_plt(ax, com,scene_scale)
 
-        # ---- ENVIRONMENT OBJECTS ----
+        return ax
+    
+    # ---- DRAW FIGURE ----
+    def draw_figure_plt(self, timestep, ax, ax2=None):
+        # Update or load state
+        if self.compute_and_animate:
+            self.update()
+        else:
+            self.load_timestep(timestep)
+
+        # Unpack ax, ax2 from FuncAnimation's wrapper
+        ax = ax[0]
+        ax2 = ax2[0]
+
+        # Print calculation progress
+        print(f"----- Animation progress: {timestep} / {self.num_timesteps} -----" ,end="\r", flush=True)
+
+        # Draw frame and graph onto existing axes
+        self.draw_frame_plt(ax)
+        if self.show_graph:
+            self.draw_graph_plt(ax2)
+    
+    # ---- DRAW FRAME ----
+    def draw_frame_plt(self, ax):
+        '''
+        Draw all objects in the simulation's current state onto a supplied matplotlib ax object.
+        '''        
+        # Decide if tracking the COM in each frame
+        com, scene_scale = None, None
+        if Particle.track_com:
+            com = Particle.centre_of_mass()
+            scene_scale = Particle.scene_scale()
+
+        # Update particle and environment artists
         ax = self.draw_environment_objects_plt(ax, com, scene_scale)
-        
-        # ---- PARTICLES ----
         ax = self.draw_particle_objects_plt(ax, com, scene_scale)
 
         return ax
@@ -214,7 +266,6 @@ class Manager:
     @staticmethod
     def default_draw_backdrop_plt(ax):
         # Default background for matplotlib frames if not specified by user
-        # Set padded limits
         ax.set_xlim(-1, Particle.env_x_lim+1)
         ax.set_ylim(-1, Particle.env_y_lim+1)
 
@@ -233,26 +284,24 @@ class Manager:
     
     def draw_particle_objects_plt(self, ax, com, scene_scale):
         # Draw all particle objects from state dictionary onto supplied Matplotlib.pyplot ax object
-        for particle_object_type, particle_objects_list in self.state["Particle"].items():
-            for particle_object in particle_objects_list:
-                particle_object.draw_plt(ax, com,scene_scale)
+        for particle in self.iterate_all_particles():
+            particle.draw_plt(ax, com,scene_scale)
         return ax
     
-    # Draw graph
+    # ---- DRAW GRAPH ----
+    def draw_graph_plt(self, ax2):
+        # Oversee drawing a graph onto an axis
+        # Add any shared functionality here
+        self.draw_graph_plt_func(ax2)
+        return ax2
+
     @staticmethod
     def default_draw_graph_plt(self, ax2):
         # Default graph if not specified by user
-        ax2.clear()
         max_time = int(self.num_timesteps)*self.delta_t
         ax2.set_xlim(0, max_time)  # Set x-axis limits
         return ax2
     
     draw_graph_plt_func = default_draw_graph_plt
 
-    def draw_graph_plt(self, ax2):
-        # Oversee drawing a graph onto an axis
-        # TODO: Add any shared functionality here
-        ax2.clear()
-        self.draw_graph_plt_func(ax2)
-        return ax2
-
+    
