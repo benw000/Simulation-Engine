b01988e56c482a6c4db65781105a8d3f9db60433
benw000 <benjamin.winstanley.007@gmail.com>
2025-06-08 11:25:55 +0100

    Removed import *, replaced with importing specific classes

--- simulation_engine/classes/__init__.py
+++ simulation_engine/classes/__init__.py
@@ -1,10 +1,9 @@
 #__init__.py
 # Controls what is imported when the classes submodule is imported
-# TODO: Change this to possibly remove *? Is this bad practice?
 
-from .parents import *
-from .birds import *
-from .evac import *
-from .nbody import *
-from .springs import *
-from .pool import *
+from .parents import Particle, Environment, Wall, Target
+from .birds import Prey, Predator
+from .evac import Human
+from .nbody import Star
+from .springs import Solid
+from .pool import Pool
